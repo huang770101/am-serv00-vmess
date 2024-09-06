0571df51ae2aa9855cbc89207f3ab119a930fab5
huang770101 <72103769+huang770101@users.noreply.github.com>
2024-09-06 19:28:12 +0800

    Update install_serv00_vmess.sh

--- install_serv00_vmess.sh
+++ install_serv00_vmess.sh
@@ -14,7 +14,7 @@ reading() { read -p "$(red "$1")" "$2"; }
 
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-export UUID=${UUID:-'d36c4d9f-31c4-45f1-8c64-102a6142001e'}
+export UUID=${UUID:-'6e7f1c53-317f-428e-8304-acb3c7a5f836'}
 export NEZHA_SERVER=${NEZHA_SERVER:-''} 
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
 export NEZHA_KEY=${NEZHA_KEY:-''} 
